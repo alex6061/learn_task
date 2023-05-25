bd34137c1f4864b337c614c11b57b1ec1f976160
John Doe <johndoe@email.com>
2023-05-25 18:25:49 +0300

    Ready getSum Function

---
+++ Functions/ToCamelCase_function/To Camel Case.js
@@ -0,0 +1,14 @@
+ /**
+  * @function ToCamelCase
+  * @typedef {string} string
+  * @param {string} str
+  * @returns {string}
+  */
+
+ export const ToCamelCase = (str) => {
+
+ }
+
+ console.log (ToCamelCase('the-stealth-warrior')) // theStealthWarrior
+
+ 
