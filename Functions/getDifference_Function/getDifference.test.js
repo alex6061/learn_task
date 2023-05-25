180d165bd53826cf3869406a2d150f8c410aacb3
John Doe <johndoe@email.com>
2023-05-25 19:53:17 +0300

    get Difference function

---
+++ Functions/getDifference_Function/getDifference.test.js
@@ -0,0 +1,9 @@
+import {getDifference} from './getDifference.js'
+
+console.log(getDifference('5', 5)) // undefined
+console.log(getDifference(5, true)) // undefined
+console.log(getDifference(-5, 5)) // undefined
+console.log(getDifference(5, -5)) // undefined
+console.log(getDifference(3, 5)) // undefined
+console.log(getDifference(5, 3)) // 2
+console.log(getDifference(5, 5)) // 0
