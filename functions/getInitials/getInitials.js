9cb7e424bee24560bcc9e3e18ff6be5d76ffe7d4
John Doe <johndoe@email.com>
2023-06-01 23:59:46 +0300

    Created function getInitials

---
+++ functions/getInitials/getInitials.js
@@ -0,0 +1,20 @@
+/**
+ * @function getInitials
+ * @param {string} name 
+ * @returns {string | undefined}
+ */
+
+export const getInitials = (name) => {
+  return (name[0][0] + '.' + name[1][0]).toUpperCase()
+};
+
+// export const getInitials = (name) => {
+//   const toUpperName = name.toUpperCase()
+//   const isNameSplit = toUpperName.split(' ');
+//   const newArr = isNameSplit.map(item => [item])
+//   const newName = newArr[0];
+//   const newSurname = newArr[1];
+//   const nameLetter = newName.toString().substring(0,1);
+//   const surnameLetter= newSurname.toString().substring(0,1);
+//   return `${nameLetter}.${surnameLetter}`;
+// };
---
+++ functions/getInitials/getInitials.test.js
@@ -0,0 +1,4 @@
+import { getInitials } from "./getInitials.js";
+
+console.log(getInitials('Sam Harris') === 'S.H');
+console.log(getInitials('patrick feeney') === 'P.F');
