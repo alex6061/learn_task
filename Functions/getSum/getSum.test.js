f65015dbe6e7daee32fe5b53639eab24ca3b15b0
a.garkusha <a.garkusha@itcsolutions.ru>
2023-06-08 19:47:54 +0300

    Сleared the branch

--- Functions/getSum/getSum.test.js
+++ Functions/getSum/getSum.test.js
@@ -1,10 +1,14 @@
 import {getSum} from './getSum.js';
 
-
-console.log(getSum('5', 5) === undefined); 
-console.log(getSum(5, true) === undefined);
-console.log(getSum(-5, 5) === undefined);
-console.log(getSum(5, -5) === undefined);
-console.log(getSum(5, 0) === undefined); 
-console.log(getSum(5, 1.5) === undefined); 
-console.log(getSum(5, 5) === 10); 
+describe('getSum', () => {
+  if('unit test 1', () => { expect(getSum('5', 5)).toEqual(undefined); });
+  if('unit test 2', () => { expect(getSum(5, true)).toEqual(undefined); });
+  if('unit test 3', () => { expect(getSum(-5, 5)).toEqual(undefined); });
+  if('unit test 4', () => { expect(getSum(5, -5)).toEqual(undefined); });
+  if('unit test 5', () => { expect(getSum(5, 0)).toEqual(undefined); });
+  if('unit test 6', () => { expect(getSum(5.3,5)).toEqual(undefined); });
+  if('unit test 7', () => { expect(getSum(5,5.3)).toEqual(undefined); });
+  if('unit test 8', () => { expect(getSum(5.3,5.3)).toEqual(undefined); });
+  if('unit test 9', () => { expect(getSum(5,5)).toEqual(10); });
+  if('unit test 10', () => { expect(getSum(2,2)).toEqual(4); });
+});
